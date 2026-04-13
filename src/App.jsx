bf0e54cdbd63d569df37a8ef36459ff22cba08ad
Angel Serrato <serrato.anggel@gmail.com>
2026-04-12 20:50:11 -0500

    feat: apply card styling, layout and design tokens

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,43 @@ import imageEquilibrium from './assets/image-equilibrium.jpg';
 
 export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center text-center text-white">
-      <article>
-        <h1>Equilibrium #3429</h1>
-        <p>Our Equilibrium collection promotes balance and calm.</p>
-        <p>0.041 ETH</p>
-        <p>3 days left</p>
+    <main className="flex min-h-screen flex-col items-center justify-center text-white">
+      <article className="w-[350px] rounded-[15px] bg-card-blue-900 p-card-300">
         <figure>
-          <img src={imageAvatar} alt="Avatar" />
+          <img
+            src={imageEquilibrium}
+            alt="Equilibrium"
+            className="mb-card-300 rounded-[8px]"
+          />
         </figure>
-        <p>Creation of Jules Wyvern</p>
+        <figure className="absolute top-0 left-0 hidden">
+          <img src={iconView} alt="View" />
+        </figure>
+        <h1 className="mb-card-200">Equilibrium #3429</h1>
+        <p className="mb-card-300 text-card-blue-500">
+          Our Equilibrium collection promotes balance and calm.
+        </p>
+        <div className="mb-card-300 flex justify-between">
+          <figure className="flex items-center gap-card-100">
+            <img src={iconEthereum} alt="Ethereum" />
+            <figcaption className="text-card-cyan-400">0.041 ETH</figcaption>
+          </figure>
+          <figure className="flex items-center gap-card-100">
+            <img src={iconClock} alt="Clock" />
+            <figcaption className="text-card-blue-500">3 days left</figcaption>
+          </figure>
+        </div>
+        <hr className="mb-card-300 h-px border-card-blue-800" />
+        <figure className="flex items-center gap-card-200">
+          <img
+            src={imageAvatar}
+            alt="Avatar"
+            className="w-[36px] rounded-full border border-white"
+          />
+          <figcaption className="flex gap-card-100 text-card-blue-500">
+            Creation of <span className="text-white">Jules Wyvern</span>
+          </figcaption>
+        </figure>
       </article>
     </main>
   );
