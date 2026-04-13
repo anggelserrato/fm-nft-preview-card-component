c14475e2b681dfe3a969991231f21b861524b3ce
Angel Serrato <serrato.anggel@gmail.com>
2026-04-12 19:19:57 -0500

    feat: implement card structure, configure prettier plugin for tailwindcss and update documentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-function App() {
+import imageAvatar from './assets/image-avatar.png';
+import iconClock from './assets/icon-clock.svg';
+import iconEthereum from './assets/icon-ethereum.svg';
+import iconView from './assets/icon-view.svg';
+import imageEquilibrium from './assets/image-equilibrium.jpg';
+
+export default function App() {
   return (
-    <div>
-      <h2 className="font-bold underline">NFT Preview Card Component</h2>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center text-center text-white">
+      <article>
+        <h1>Equilibrium #3429</h1>
+        <p>Our Equilibrium collection promotes balance and calm.</p>
+        <p>0.041 ETH</p>
+        <p>3 days left</p>
+        <figure>
+          <img src={imageAvatar} alt="Avatar" />
+        </figure>
+        <p>Creation of Jules Wyvern</p>
+      </article>
+    </main>
   );
 }
-
-export default App;
