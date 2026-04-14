9ecdebc489e6cfcfea2f8f23f3cdf0ecfa336c89
Angel Serrato <serrato.anggel@gmail.com>
2026-04-13 20:44:45 -0500

    feat: refine typography and font styles for nft preview card

--- src/App.jsx
+++ src/App.jsx
@@ -18,18 +18,24 @@ export default function App() {
         <figure className="absolute top-0 left-0 hidden">
           <img src={iconView} alt="View" />
         </figure>
-        <h1 className="mb-card-200">Equilibrium #3429</h1>
-        <p className="mb-card-300 text-card-blue-500">
+        <h1 className="mb-card-200 text-[22px] font-semibold">
+          Equilibrium #3429
+        </h1>
+        <p className="mb-card-300 text-[18px] leading-[26px] font-light text-card-blue-500">
           Our Equilibrium collection promotes balance and calm.
         </p>
         <div className="mb-card-300 flex justify-between">
           <figure className="flex items-center gap-card-100">
             <img src={iconEthereum} alt="Ethereum" />
-            <figcaption className="text-card-cyan-400">0.041 ETH</figcaption>
+            <figcaption className="text-[16px] leading-normal font-semibold text-card-cyan-400">
+              0.041 ETH
+            </figcaption>
           </figure>
           <figure className="flex items-center gap-card-100">
             <img src={iconClock} alt="Clock" />
-            <figcaption className="text-card-blue-500">3 days left</figcaption>
+            <figcaption className="text-[16px] leading-normal font-normal text-card-blue-500">
+              3 days left
+            </figcaption>
           </figure>
         </div>
         <hr className="mb-card-300 h-px border-card-blue-800" />
@@ -39,8 +45,11 @@ export default function App() {
             alt="Avatar"
             className="w-[36px] rounded-full border border-white"
           />
-          <figcaption className="flex gap-card-100 text-card-blue-500">
-            Creation of <span className="text-white">Jules Wyvern</span>
+          <figcaption className="flex gap-card-100 text-[16px] leading-normal text-card-blue-500">
+            Creation of{' '}
+            <span className="text-[16px] leading-normal font-normal text-white">
+              Jules Wyvern
+            </span>
           </figcaption>
         </figure>
       </article>
